8bc1084016ceb752cae62340a92e2a82d611d48b
Gabriel Oliveira <gabrieloliveira30p@gmail.com>
2026-02-04 15:39:24 -0300

    bug: procurando erro de conexão entre backend e frontend.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 
 app.use(cors({
-  origin: "https://seu-link-do-marketplace.vercel.app",
+  origin: "https://nexbuy-frontend-omega.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
